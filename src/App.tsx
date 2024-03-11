89d0691077aad1aa7087257b27a0752f6fab0319
azufb <curiousazunas@gmail.com>
2024-03-11 21:03:30 +0900

    とりあえずフォーム作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { TodoForm } from './components/todoForm'
 
 function App() {
 
   return (
     <>
       <h1>Go! React! Todo App!</h1>
+      <TodoForm />
     </>
   )
 }
--- src/components/todoForm/index.tsx
+++ src/components/todoForm/index.tsx
@@ -1,5 +1,29 @@
+import { useForm } from "react-hook-form"
+
+type InputType = {
+    todo: string;
+}
+
 export const TodoForm = () => {
+    const { register, handleSubmit, reset } = useForm<InputType>();
+
+    const submitForm = (data: InputType): void => {
+        // バリデーション
+        console.log(data);
+
+        // inputをリセット
+        reset();
+    }
+
     return (
-        <></>
+        <form onSubmit={handleSubmit(submitForm)}>
+            <div>
+                <label htmlFor="todo">Todo</label>
+                <input type="text" title="todo" {...register("todo")} placeholder="タスクを入力してください" />
+            </div>
+            <div>
+                <button type="submit">追加</button>
+            </div>
+        </form>
     )
 }
